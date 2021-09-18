efc27c51a2bcd94512f67d762aa28c3616bc69f3
GROSSI <francesco.grossi@accenture.com>
2021-09-18 09:17:54 +0200

    first commit

---
+++ src/main/java/com/accenture/devops/app01web/App01WebApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.devops.app01web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App01WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App01WebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/devops/app01web/App01WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.devops.app01web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App01WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
